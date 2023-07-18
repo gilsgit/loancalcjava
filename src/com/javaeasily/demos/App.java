3fab139e766ef02b89bdcb06f866b960ca83d56d
Harry Gils <codeo@lampstand.ca>
2023-07-18 15:20:45 -0400

    added more functionality

--- src/com/javaeasily/demos/App.java
+++ src/com/javaeasily/demos/App.java
@@ -2,12 +2,21 @@ package com.javaeasily.demos;
 
 public class App {
     public static void main(String[] args) {
-        double amount = 1000;
+        double amount = 100;
         int years = 5;
-        double interestRate = 1.5;
+        double interestRate = 10.0;
+
+        double interestRateMultiplier = 1 + interestRate / 100;
+
+        double year1AmountDue = amount * interestRateMultiplier;
+        double year2AmountDue = year1AmountDue * interestRateMultiplier;
+        double year3AmountDue = year2AmountDue * interestRateMultiplier;
+        double year4AmountDue = year3AmountDue * interestRateMultiplier;
+        double year5AmountDue = year4AmountDue * interestRateMultiplier;
 
 
-        System.out.println("Loan Calculator");
+        System.out.println("Loan Calculator\nTotal Amount Due in 5 Years is:");
+        System.out.println(year5AmountDue);
 
 
     }

11b117f0d32d782c7cc9cf88a4674f0d607c97c1
Hello Good <codeo@lampstand.ca>
2023-07-18 14:34:16 -0400

    Loan calculator project in Java from beginners course

---
+++ src/com/javaeasily/demos/App.java
@@ -0,0 +1,14 @@
+package com.javaeasily.demos;
+
+public class App {
+    public static void main(String[] args) {
+        double amount = 1000;
+        int years = 5;
+        double interestRate = 1.5;
+
+
+        System.out.println("Loan Calculator");
+
+
+    }
+}

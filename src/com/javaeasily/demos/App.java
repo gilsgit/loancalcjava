5c2cc3935ab365f08a8a15007ab86f8df493e8fc
Harry Gils <codeo@lampstand.ca>
2023-08-04 15:23:32 -0400

    refactored into methods

--- src/com/javaeasily/demos/App.java
+++ src/com/javaeasily/demos/App.java
@@ -11,27 +11,36 @@ public class App {
         double interestRate = 10;
 
         if (amount > 0 && years > 0 && interestRate > 0 ) {
-            System.out.println("Calculating loan based on:");
-            System.out.println("Initial Amount:  " + "$" + amount);
-            System.out.println("Number of years: " + years);
-            System.out.println("Interest Rate:   " + interestRate + "%");
-
-            double interestRateMultiplier = 1 + interestRate / 100;
-
-            double currentAmountPayable = amount;
-            int currentYear= 1;
-            while (currentYear <= years) {
-                currentAmountPayable =  currentAmountPayable * interestRateMultiplier;
-                currentYear++;
-            }
-
-            String totalAmountDue = Double.toString(currentAmountPayable);
-            int indexOfDecimalPoint = totalAmountDue.indexOf(".");
-            String totalAmountDueFormatted = totalAmountDue.substring(0, indexOfDecimalPoint + 3);
-            System.out.println("Total Amount Due: " + totalAmountDueFormatted);
-
+            printInputs(amount, years, interestRate);
+            printResult(calculatePaymentAmount(amount, years, interestRate));
         } else {
             System.out.println("Invalid values -  can not calculate repayment amount.");
         }
+    }
+
+    private static void printResult(double currentAmountPayable) {
+        String totalAmountDue = Double.toString(currentAmountPayable);
+        int indexOfDecimalPoint = totalAmountDue.indexOf(".");
+        String totalAmountDueFormatted = totalAmountDue.substring(0, indexOfDecimalPoint + 3);
+        System.out.println("Total Amount Due: " + totalAmountDueFormatted);
+    }
+
+    private static double calculatePaymentAmount(int amount, int years, double interestRate) {
+        double interestRateMultiplier = 1 + interestRate / 100;
+
+        double currentAmountPayable = amount;
+        int currentYear= 1;
+        while (currentYear <= years) {
+            currentAmountPayable =  currentAmountPayable * interestRateMultiplier;
+            currentYear++;
+        }
+        return currentAmountPayable;
+    }
+
+    private static void printInputs(int amount, int years, double interestRate) {
+        System.out.println("Calculating loan based on:");
+        System.out.println("Initial Amount:  " + "$" + amount);
+        System.out.println("Number of years: " + years);
+        System.out.println("Interest Rate:   " + interestRate + "%");
     }
 }
